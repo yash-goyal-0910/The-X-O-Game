d5f0a9498eae3974bf49618f1b0f5f5b5fff610a
yash-goyal-0910 <yashgoyal09102005gmail.com>
2024-09-14 08:55:23 +0530

    Initial Files

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "help.cpp"
+using namespace std;
+int main(){
+    cout<<"Hello";
+    prnt();
+    return 0;
+}
---
+++ help.cpp
@@ -0,0 +1,4 @@
+
+void prnt(){
+    std::cout<<"help";
+}
